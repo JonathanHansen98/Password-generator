401596893bf9fe8cddf83c9df92238b819dc939a
Jonathan Hansen <kriah0872@gmail.com>
2020-03-17 09:24:04 -0700

    first bit of work

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,47 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-const randomFunc = {
-  passwordLength: length,
-  lower: hasLower,
-  upper: hasUpper,
-  symbols: hasSym,
-  numbers:  hasNum,
-}
+var hasLower;
+var hasUpper;
+var hasNum;
+var hasSym;
+
 
 // Write password to the #password input
-function writePassword(lower, upper, symbols, numbers, length) {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function writePassword() {
+  var length = prompt("Length: Please choose a number between 8 and 128.");
+
+  if (length < 8 || length > 128) {
+    alert("Please choose a number between 8 and 128.")
+    length = prompt("Length: Please choose a number between 8 and 128.");
+  };
+
+  hasLower = confirm("Include lower case?");
+  hasUpper = confirm("Include upper case?");
+  hasNum = confirm("Include numbers?");
+  hasSym = confirm("Include Symbols?");
+  password = generatePassword(hasLower, hasUpper, hasNum, hasSym, length);
+  passwordText = document.querySelector("#password");
+
+  console.log(length);
+  console.log(hasLower);
+  console.log(hasUpper);
+  console.log(hasNum);
+  console.log(hasSym);
+
 
   passwordText.value = password;
-
-  
-  const typesCount = lower + upper + symbols + numbers;
-
-  console.log(typesCount)
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var length = prompt("Length: Please choose a number between 8 and 128.");
-var hasLower = confirm("Include lower case?");
-var hasUpper = confirm("Include upper case?");
-var hasNum = confirm("Include numbers?");
-var hasSym = confirm("Include Symbols?");
-
-console.log(length);
-console.log(hasLower);
-console.log(hasUpper);
-console.log(hasNum);
-console.log(hasSym);
-
-
-
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
+
 
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -54,4 +52,17 @@ function getRandomSymbol() {
 
 function getRandomNumber() {
   return Math.floor(Math.random() * 10)
+}
+
+function generatePassword(low, up, num, sym, length) {
+  var count = low + up + num + sym;
+  var typeArray = [{ low }, { up }, { num }, { sym }].filter( check => Object.values(check)[0])
+  console.log(typeArray)
+
+  for (let index = 0; index < length; index += count) {
+    typeArray.forEach(type => {
+      var func = Object.keys(type)[0];
+      console.log(func)
+    })
+  }
 }
