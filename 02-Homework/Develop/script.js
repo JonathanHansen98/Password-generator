8736c6f2c076de662bcc5926928b8453aa22b91b
Jonathan Hansen <kriah0872@gmail.com>
2020-03-18 09:30:02 -0700

    work work work

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,36 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordText = ""
+
 var hasLower;
 var hasUpper;
 var hasNum;
 var hasSym;
 
+var pwGenerator = {
+  low: getRandomLower,
+  up: getRandomUpper,
+  num: getRandomNumber,
+  sym: getRandomSymbol
+};
+
 
 // Write password to the #password input
 function writePassword() {
-  var length = prompt("Length: Please choose a number between 8 and 128.");
+  var length = parseInt(prompt("Length: Please choose a number between 8 and 128."));
 
-  if (length < 8 || length > 128) {
+  while (length < 8 || length > 128) {
     alert("Please choose a number between 8 and 128.")
     length = prompt("Length: Please choose a number between 8 and 128.");
-  };
+  }
 
   hasLower = confirm("Include lower case?");
   hasUpper = confirm("Include upper case?");
   hasNum = confirm("Include numbers?");
   hasSym = confirm("Include Symbols?");
-  password = generatePassword(hasLower, hasUpper, hasNum, hasSym, length);
-  passwordText = document.querySelector("#password");
-
-  console.log(length);
-  console.log(hasLower);
-  console.log(hasUpper);
-  console.log(hasNum);
-  console.log(hasSym);
-
-
-  passwordText.value = password;
 }
 
 // Add event listener to generate button
@@ -56,13 +54,19 @@ function getRandomNumber() {
 
 function generatePassword(low, up, num, sym, length) {
   var count = low + up + num + sym;
-  var typeArray = [{ low }, { up }, { num }, { sym }].filter( check => Object.values(check)[0])
+  var typeArray = [{ low }, { up }, { num }, { sym }].filter(check => Object.values(check)[0])
   console.log(typeArray)
 
   for (let index = 0; index < length; index += count) {
     typeArray.forEach(type => {
       var func = Object.keys(type)[0];
-      console.log(func)
+
+      passwordText += pwGenerator[func]();
+
+      return passwordText;
     })
+
+    passwordText = document.querySelector("#password");
+    console.log(passwordText);
   }
 }
