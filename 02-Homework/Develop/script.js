217dcd5606c7bd55a41f7d3bd964bb1d252886f9
Jonathan Hansen <kriah0872@gmail.com>
2020-03-19 00:41:39 -0700

    FREAKIN WORKS YO

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,8 +1,4 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-var passwordText = ""
-
+// Declaring Global variables and pwGenerator object
 var hasLower;
 var hasUpper;
 var hasNum;
@@ -15,25 +11,32 @@ var pwGenerator = {
   sym: getRandomSymbol
 };
 
-
-// Write password to the #password input
-function writePassword() {
-  var length = parseInt(prompt("Length: Please choose a number between 8 and 128."));
-
-  while (length < 8 || length > 128) {
-    alert("Please choose a number between 8 and 128.")
-    length = prompt("Length: Please choose a number between 8 and 128.");
-  }
-
-  hasLower = confirm("Include lower case?");
-  hasUpper = confirm("Include upper case?");
-  hasNum = confirm("Include numbers?");
-  hasSym = confirm("Include Symbols?");
-}
+// Assignment Code
+var password = ""
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Write password to the #password input
+function writePassword() {
+  var length = parseInt(prompt("Length: Please choose a number between 8 and 128."));
+// While loop keeps users from advancing until they select a number between 8 and 128
+  while (length < 8 || length > 128) {
+    alert("Please choose a number between 8 and 128.")
+    length = prompt("Length: Please choose a number between 8 and 128.");
+  };
+  hasLower = confirm("Include lower case?");
+  hasUpper = confirm("Include upper case?");
+  hasNum = confirm("Include numbers?");
+  hasSym = confirm("Include Symbols?");
+  password = generatePassword(hasLower, hasUpper, hasNum, hasSym, length);
+  passwordText.value = password;
+};
+
+
+// Create random characters functions
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
@@ -52,21 +55,29 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 10)
 }
 
+
+
+
+
+// Generate Password Function
+
+// passing in functions from pwGenerator Object
 function generatePassword(low, up, num, sym, length) {
+  // counts criteria selected, filters out fasle values and creates a new array with only the new values
   var count = low + up + num + sym;
   var typeArray = [{ low }, { up }, { num }, { sym }].filter(check => Object.values(check)[0])
   console.log(typeArray)
 
+// For loop increments at the count of how many criteria was selected
   for (let index = 0; index < length; index += count) {
+
+// The forEach method executes a function once for each array element, I use it here to create variable 'func' equal to the keys in our typeArray concatenated onto the pwGenerator object. This calls our functions one after the other for the length of our password, creating our random password.
     typeArray.forEach(type => {
       var func = Object.keys(type)[0];
+      password += pwGenerator[func]();
 
-      passwordText += pwGenerator[func]();
-
-      return passwordText;
     })
-
-    passwordText = document.querySelector("#password");
-    console.log(passwordText);
   }
+  console.log(password)
+  return password;
 }
